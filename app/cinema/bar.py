28fd60579113c8c9b89ad70fc0c7302635c23f1b
Ivan Synytskyi <ivansinickij29@icloud.com>
2026-03-27 10:12:53 +0100

    flake8

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,4 @@
 class CinemaBar:
     @staticmethod
-    def sell_product(product, customer):
-        print (f"Cinema bar sold {product} to {customer.name}")
+    def sell_product(product: str, customer) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,10 +1,14 @@
+from app.people.cinema_staff import Cleaner
+
+
 class CinemaHall:
-    def __init__(self, number):
+    def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name, customers, cleaning_staff):
+    def movie_session(self, movie_name: str,
+                      customers: list, cleaning_staff: Cleaner) -> None:
         print (f"Movie {movie_name} started")
         for customer in customers:
             customer.watch_movie(movie_name)
         print (f"Movie {movie_name} ended")
-        cleaning_staff.cleaning_hall(movie_name)
+        cleaning_staff.clean_hall(self.number)
